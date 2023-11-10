4640882f3334b7c9613f079e50c66f26901b85f8
aphilipharrell <haphilip@umich.edu>
2023-11-10 10:41:23 -0500

    Filter on management memberships

--- mogc_partnerships/pipeline.py
+++ mogc_partnerships/pipeline.py
@@ -8,17 +8,26 @@ from openedx_filters.learning.filters import CourseEnrollmentStarted
 from .models import CohortOffering, Partner, PartnerOffering
 
 
+def user_can_access_course(user, course_key):
+    partner = Partner.objects.get(org=course_key.org)
+    offering = partner.offerings.get(course_key=course_key)
+    cohort_ids = CohortOffering.objects.filter(offering=offering).values_list(
+        "cohort_id", flat=True
+    )
+    user_in_cohort = user.memberships.filter(cohort__in=cohort_ids).exists()
+    user_is_partner_manager = user.management_memberships.filter(
+        user_id=user.id, partner_id=partner.id
+    ).exists()
+    if user_in_cohort or user_is_partner_manager:
+        return True
+
+
 class MembershipRequiredEnrollment(PipelineStep):
     """Prevents non-members from enrolling in partner courses."""
 
     def run_filter(self, user, course_key, mode):
         try:
-            partner = Partner.objects.get(org=course_key.org)
-            offering = partner.offerings.get(course_key=course_key)
-            cohort_ids = CohortOffering.objects.filter(offering=offering).values_list(
-                "cohort_id", flat=True
-            )
-            if user.memberships.filter(cohort__in=cohort_ids).exists() or user.is_staff:
+            if user_can_access_course(user, course_key):
                 return {}
             raise CourseEnrollmentStarted.PreventEnrollment(
                 "This course requires a partner membership."
@@ -46,12 +55,7 @@ class HidePartnerCourseAboutPages(PipelineStep):
             "course-v1:" + "+".join([org, course_id, run])
         )  # TODO: Find a better way to do this.
         try:
-            partner = Partner.objects.get(org=org)
-            offering = partner.offerings.get(course_key=course_key)
-            cohort_ids = CohortOffering.objects.filter(offering=offering).values_list(
-                "id", flat=True
-            )
-            if user.memberships.filter(cohort__in=cohort_ids).exists():
+            if user_can_access_course(user, course_key):
                 return {}
             raise Http404
         except PartnerOffering.DoesNotExist:
