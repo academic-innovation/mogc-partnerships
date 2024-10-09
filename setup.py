fc09e0d63ca6b293f5661850f1696b9eb21e6a33
Michael Wheeler <mpwheel@umich.edu>
2024-10-09 14:14:30 -0400

    Bump to v0.4.4

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="mogc-partnerships",
-    version="0.4.3",
+    version="0.4.4",
     packages=find_packages(),
     install_requires=[
         "django>=3.2,<4.0",
