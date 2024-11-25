6d9f22a3a5684b86a9fb6d0a4a506cc9e1f1939b
Dennis O'Reilly <doreilly@umich.edu>
2024-11-25 17:13:55 -0500

    Open up package Django dependency declaration - Bump version to 0.4.7b1

--- setup.py
+++ setup.py
@@ -2,10 +2,10 @@ from setuptools import find_packages, setup
 
 setup(
     name="mogc-partnerships",
-    version="0.4.6",
+    version="0.4.7b1",
     packages=find_packages(),
     install_requires=[
-        "django>=3.2,<4.0",
+        "django>=3.2,<5.0",
         "edx-django-utils",
         "openedx-events>=0.8.1",
         "openedx-filters>=0.7.0",
